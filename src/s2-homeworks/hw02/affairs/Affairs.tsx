1393aa1cf1c79fb5d7d19bd824aceab2831e750e
Intrstng <intrstg@gmail.com>
2023-12-13 23:00:57 +0300

    feat: complete Tuesday - second week homework task

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -4,24 +4,24 @@ import {AffairType, FilterType} from '../HW2'
 import s from './Affairs.module.css'
 
 type AffairsPropsType = {
-    data: any // need to fix any
-    setFilter: any
-    deleteAffairCallback: any
+    data: AffairType[] // need to fix any
+    setFilter: (filter: FilterType) => void
+    deleteAffairCallback: (_id: number) => void
     filter: FilterType
 }
 
 function Affairs(props: AffairsPropsType) {
     const setAll = () => {
-        // need to fix
+        props.setFilter('all')// need to fix
     }
     const setHigh = () => {
-        // need to fix
+        props.setFilter('high')// need to fix
     }
     const setMiddle = () => {
-        // need to fix
+        props.setFilter('middle')// need to fix
     }
     const setLow = () => {
-        // need to fix
+        props.setFilter('low')// need to fix
     }
 
     const cnAll = s.button + ' ' + s.all + (props.filter === 'all' ? ' ' + s.active : '')
@@ -32,6 +32,7 @@ function Affairs(props: AffairsPropsType) {
     const mappedAffairs = props.data.map((a: AffairType) => (
         <Affair
             key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
+                                                id={a._id}
             affair={a}
             deleteAffairCallback={props.deleteAffairCallback}
         />
